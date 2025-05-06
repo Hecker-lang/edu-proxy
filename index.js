792778c88ce1d45998c7f6189e4f61367951ed34
Hecker-lang <pablohecker.123@gmail.com>
2025-05-06 14:15:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ app.use(
       followRedirect: true,
       maxRedirects: 10
     },
-    block: []  // keine Blocking‑Regeln
+    block: []
   })
 );
 
 // Startseite mit Nutzungshinweis
 app.get('/', (req, res) => {
   const host = req.get('host');
-  res.send(`
-    <h1>Render‑Proxy</h1>
-    <p>Nutze <code>/proxy/&lt;URL&gt;</code>, z. B.:</p>
-    <pre>https://${host}/proxy/https://example.com</pre>
-  `);
+  res.send(
+    '<h1>Render-Proxy</h1>' +
+    '<p>Nutze <code>/proxy/&lt;URL&gt;</code>, z. B.:</p>' +
+    '<pre>https://' + host + '/proxy/https://example.com</pre>'
+  );
 });
 
 app.listen(PORT, () => {
-  console.log(\`Server läuft auf Port \${PORT}\`);
+  console.log('Server läuft auf Port ' + PORT);
 });
